ea8e25d1caa10d4a5a41645cb3f1cbcfb0dccddb
Francis Anyuru <anyuruf@gmail.com>
2022-07-15 06:20:06 -0400

    first commit

---
+++ discovery-service/src/test/java/net/anyuruf/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.anyuruf.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ members_basic/src/test/java/net/anyuruf/members_basic/MembersBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package net.anyuruf.members_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembersBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
