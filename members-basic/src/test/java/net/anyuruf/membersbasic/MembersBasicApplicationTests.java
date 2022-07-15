e37b006fb6f5ea3fcf33267390879a3302f29192
Francis Anyuru <anyuruf@gmail.com>
2022-07-15 07:06:43 -0400

    Down grade to java 11a

---
+++ members-basic/src/test/java/net/anyuruf/membersbasic/MembersBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package net.anyuruf.membersbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembersBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
