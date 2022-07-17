1658f79de6a7c2f002a67f0b6f8aecc7eeb95e30
anyuruf <anyuruf@gmail.com>
2022-07-17 15:43:46 +0000

    Feat  Command runner bean, Controller n Repository

--- members-basic/src/main/java/net/anyuruf/membersbasic/MembersBasicApplication.java
+++ members-basic/src/main/java/net/anyuruf/membersbasic/MembersBasicApplication.java
@@ -6,8 +6,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MembersBasicApplication {
 
+	private final static Logger log = LoggerFactory.getLogger(MembersBasicApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(MembersBasicApplication.class, args);
 	}
+
+	@Bean
+  	public CommandLineRunner clr(MemberBasicRepository memberRepository) {
+    return args -> {
+      memberRepository.deleteAll();
+      
+      MemberBasic john = new MemberBasic(UUID.randomUUID(), "Jenesio Omodo", "Anyuru", LocalDate.of(1927, Month.NOVEMBER, 11), new HashSet<>());
+      MemberBasic jane = new MemberBasic(UUID.randomUUID(), "Anna Mary", "Kababito", new HashSet<>());
+      
+      Vet savedJohn = memberRepository.save(john);
+      Vet savedJane = memberRepository.save(jane);
+
+      memberRepository.findAll()
+        .forEach(v -> log.info("Vet: {}", v.getFirstName()));
+      
+      memberRepository.findById(savedJohn.getId())
+        .ifPresent(v -> log.info("Vet by id: {}", v.getFirstName()));
+    };
+  }
 
 }
--- members-basic/src/main/java/net/anyuruf/membersbasic/service/MemberBasicRepository.java
+++ members-basic/src/main/java/net/anyuruf/membersbasic/service/MemberBasicRepository.java
@@ -1,5 +1,7 @@
 import org.springframework.data.cassandra.repository.ReactiveCassandraRepository;
 import java.util.UUID;
 
+import net.anyuruf.entity.MemberBasic
+
 interface MemberBasicRepository extends ReactiveCassandraRepository<MemberBasic, UUID> { 
 }
